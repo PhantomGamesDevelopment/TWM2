3b5aa5d67e923838071f507a6756c4398aa6a6c0
Robert Fritzen <rfritzen@phantomdev.net>
2017-06-26 19:37:47 -0500

    Update readme

--- scripts/Customize/CustomScripts.cs
+++ scripts/Customize/CustomScripts.cs
@@ -11,15 +11,15 @@
 
 //Add custom chat commands here!
 //I've included a little sample command (in comments)
-function parseCustomCommands(%sender, %command, %args) {
-   switch$(strLwr(%command)) {   //CAUTION! Note the strlwr here! that means your command must show in lowercase in each case "": statement
+//function parseCustomCommands(%sender, %command, %args) {
+//   switch$(strLwr(%command)) {   //CAUTION! Note the strlwr here! that means your command must show in lowercase in each case "": statement
       //case "samplecommand":
       //   %arg1 = getWord(%args, 0);
       //   %arg2 = getWord(%args, 1)
       //   //Do stuff here!
-      //   return 1; //return 1; - Command executed correctly
-   }
-}
+      //   return 1; //return 1; - Command executed correctly\
+//      default:
+//   }
+//}
 //make sure you add all of your commands to the list by doing this:
 //addCMD("Custom", "sampleCommand", "Usage: /sampleCommand [arg1] [arg2]: Sample command ftw! people see this when they type /cmdHelp sampleCommand.");
-
